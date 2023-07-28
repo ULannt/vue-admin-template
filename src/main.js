e7f39f74d96d9c1f63262df4f727887bb1dd1c61
ULannt <galaxy.nebula2021@gmail.com>
2023-07-28 15:13:17 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import "normalize.css/normalize.css"
 import ElementUI from "element-ui"
 import "element-ui/lib/theme-chalk/index.css"
 
-import "@/styles/index.scss" // global css
+import "@/styles/index.scss"
 
 import App from "./App"
 import store from "./store"
